fa97235b736fdb7605354f697e1e79ca2ce7ffdf
Adefolaoluwayan Adeoye <adefolaadeoye@gmail.com>
2025-11-04 20:53:40 +0000

    add app switch test

--- test/views/widget_test.dart
+++ test/views/widget_test.dart
@@ -81,6 +81,21 @@ void main() {
       await tester.pump();
       expect(find.text('Note: Extra mayo'), findsOneWidget);
     });
+
+    testWidgets('toggles switch between footlong and six-inch',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(const App());
+      expect(find.textContaining('footlong sandwich'), findsOneWidget);
+
+      final switchFinder = find.byType(Switch);
+      await tester.tap(switchFinder);
+      await tester.pumpAndSettle();
+      expect(find.textContaining('six-inch sandwich'), findsOneWidget);
+
+      await tester.tap(switchFinder);
+      await tester.pumpAndSettle();
+      expect(find.textContaining('footlong sandwich'), findsOneWidget);
+    });
   });
 
   group('StyledButton', () {
